82b3e3684dd87a9cace168593ee1f076cd87a9d9
Kassoum10 <devalkaabi@gmail.com>
2023-02-06 09:01:08 +0000

    Question 3

---
+++ database/migrations/2023_02_05_141149_create_comptes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comptes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('abonne_id')->unsigned();
+            $table->foreign('abonne_id')->references('id')->on('abonne')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('libelle');
+            $table->string('description');
+            $table->int('agence');
+            $table->string('banque');
+            $table->string('libelle');
+            $table->int('numero');
+            $table->int('rib');
+            $table->float('montant');
+            $table->string('domiciliation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comptes');
+    }
+};
